48737cbaf5fd8367355bbcf8e23f73a2e725d182
kholodalosimi <khlaood22@gmail.com>
2022-01-23 12:30:06 +0300

    BackEnd2

--- src/main/java/com/example/plantms/controller/UsersController.java
+++ src/main/java/com/example/plantms/controller/UsersController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
+@CrossOrigin(origins = "https://plant-friend-frontend.herokuapp.com/")
+
 @RestController
 @RequestMapping(path = "api/users")
 
